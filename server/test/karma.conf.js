56b978126d524d4f5041637ff42063fa8581fd36
Vasile Sambor <vasilesambor@gmail.com>
2018-01-12 23:30:40 +0100

    [#8] - Fix backend unit tests

--- server/test/karma.conf.js
+++ server/test/karma.conf.js
@@ -1,14 +1,15 @@
 // This is a karma config file. For more details see http://karma-runner.github.io/0.13/config/configuration-file.html
 
-module.exports = function(config) {
+module.exports = (config) => {
   config.set({
     browsers: ['PhantomJS'],
-    frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
+    frameworks: ['mocha', 'sinon-chai'],
     reporters: ['spec', 'coverage'],
-    files: [
-      '../../node_modules/babel-polyfill/dist/polyfill.js',
-      './specs/*.spec.js'
-    ],
+    files: ['./specs/*.spec.js'],
+    preprocessors: {
+      // Allows usage of ES6 in all server tests.
+      './specs/*.spec.js': 'babel'
+    },
     coverageReporter: {
       dir: './coverage',
       reporters: [
