9a730b4a2f91c568ba851b9ef77788fa1f74c52b
Vasile Sambor <vasilesambor@gmail.com>
2018-01-18 23:57:48 +0100

    [#25] - Add login page

--- client/src/i18n/en.js
+++ client/src/i18n/en.js
@@ -2,6 +2,15 @@ module.exports = {
   general: {
     hello: 'Hello',
     yes: 'Yes',
-    no: 'No'
+    no: 'No',
+    login: 'Login',
+    reset: 'Reset',
+    register: 'Register',
+    forgot_password: 'Forgot Password'
+  },
+  attributes: {
+    email: 'E-mail',
+    password: 'Password'
+
   }
 }
--- client/src/i18n/fr.js
+++ client/src/i18n/fr.js
@@ -2,6 +2,15 @@ module.exports = {
   general: {
     hello: 'Bonjour',
     yes: 'Oui',
-    no: 'Non'
+    no: 'Non',
+    login: 'Se connecter',
+    reset: 'Réinitialiser',
+    register: 'Inscription',
+    forgot_password: 'Mot de passe oublié'
+  },
+  attributes: {
+    email: 'E-mail',
+    password: 'Mot de passe'
+
   }
 }
