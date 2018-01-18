463061466d5d0a315c1c6d49d450f331ad363897
Vasile Sambor <vasilesambor@gmail.com>
2018-01-18 23:57:48 +0100

    [#25] - Add login page

--- client/src/components/public/Login.vue
+++ client/src/components/public/Login.vue
@@ -1,95 +1,59 @@
 <template>
-  <section class="hero is-fullheight">
-    <div class="hero-body">
-      <div class="container has-text-centered">
-        <div class="column is-4 is-offset-4">
-          <h3 class="title has-text-grey">Login</h3>
-          <p class="subtitle has-text-grey">Please login to proceed.</p>
-          <div class="box">
-            <figure class="avatar">
-              <img src="https://s.gravatar.com/avatar/5c71cbf44a48f67501a8a5c43d6c3cb2?s=80">
-            </figure>
-            <form>
-              <div class="field">
-                <div class="control">
-                  <input class="input" type="email" placeholder="Your Email" autofocus="">
-                </div>
-              </div>
+  <div class="row justify-center">
+    <div class="col-12 col-sm-8 col-md-5">
+      <q-card>
+        <q-card-title>{{$t('general.login')}}</q-card-title>
+        <q-card-separator />
+        <q-card-main>
+          <form>
+            <q-field icon="mail" :error="errors.has('email')" :error-label="errors.first('email')">
+              <q-input type="email" v-model="email" v-validate="'required|email'" name="email" :float-label="star($t('attributes.email'))" />
+            </q-field>
 
-              <div class="field">
-                <div class="control">
-                  <input class="input" type="password" placeholder="Your Password">
-                </div>
-              </div>
-              <!-- <div class="field">
-                <label class="checkbox">
-                  <input type="checkbox"> Remember me
-                </label>
-              </div> -->
-              <a class="button is-block is-info">Login</a>
-            </form>
+            <q-field icon="lock" :error="errors.has('password')" :error-label="errors.first('password')">
+              <q-input type="password" v-model="password" v-validate="'required|min:5'" name="password" :float-label="star($t('attributes.password'))"></q-input>
+            </q-field>
+          </form>
+        </q-card-main>
+
+        <q-card-separator/>
+
+        <q-card-actions>
+          <q-btn @click="validateAndSubmit" icon-right="forward" :color="$store.getters.currentTheme" type="submit" class="full-width">{{$t('general.login')}}</q-btn>
+          <div class="row full-width justify-center mt-10">
+            <q-btn flat color="primary" @click="resetAndClearErrors">{{$t('general.reset')}}</q-btn>
+            <q-btn flat color="primary">{{$t('general.register')}}</q-btn>
+            <q-btn flat color="primary">{{$t('general.forgot_password')}}</q-btn>
           </div>
-          <p class="">
-            <router-link to="Register">Sign Up</router-link> <span class="has-text-black"> &nbsp;·&nbsp; </span>
-            <a href="../">Forgot Password</a> 
-          </p>
-        </div>
-      </div>
+        </q-card-actions>
+      </q-card>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
-// import axios from "axios";
+import FormMixin from 'mixin/FormMixin'
 
 export default {
+  mixins: [FormMixin],
   data() {
-    return {}
+    return {
+      email: '',
+      password: ''
+    }
   },
   methods: {
-    onLogin() {}
+    submitForm() {
+      console.log('submitted!')
+    },
+    resetForm() {
+      Object.assign(this.$data, this.$options.data.apply(this))
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-html,
-body {
-  font-family: 'Open Sans', serif;
-  font-size: 14px;
-  font-weight: 300;
-}
-.hero.is-success {
-  background: #f2f6fa;
-}
-.hero .nav,
-.hero.is-success .nav {
-  -webkit-box-shadow: none;
-  box-shadow: none;
-}
-.box {
-  margin-top: 5rem;
-}
-.avatar {
-  margin-top: -70px;
-  padding-bottom: 20px;
-}
-.avatar img {
-  padding: 5px;
-  background: #fff;
-  border-radius: 50%;
-  -webkit-box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1),
-    0 0 0 1px rgba(10, 10, 10, 0.1);
-  box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
-}
-input {
-  font-weight: 300;
-}
-p {
-  font-weight: 700;
-}
-p.subtitle {
-  padding-top: 1rem;
-}
+
 </style>
