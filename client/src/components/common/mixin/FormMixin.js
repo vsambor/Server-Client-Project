bc0655cfd411f20468e76654ccae6784c600ef5a
Vasile Sambor <vasilesambor@gmail.com>
2018-01-20 14:29:10 +0100

    [#20] - Add vee-validate as frontend form validator

--- client/src/components/common/mixin/FormMixin.js
+++ client/src/components/common/mixin/FormMixin.js
@@ -10,22 +10,33 @@ export default {
     /**
      * Validates the form and calls submit if there is no error.
      */
-    validateBeforeSubmit() {
-      // this.$validator.validateAll().then(isValid => {
-      //   if (isValid && !this.errors.any()) {
-      //     this.submitForm();
-      //   }
-      // });
+    validateAndSubmit() {
+      this.$validator.validateAll().then(isValid => {
+        if (isValid && !this.errors.any()) {
+          this.submitForm()
+        }
+      })
     },
 
     /**
      * Resets the form data from within the component and after clears the errors on next tick.
      */
     resetAndClearErrors() {
-      // if (this.resetForm) {
-      //   this.resetForm();
-      // }
-      // this.$nextTick(() => this.errors.clear());
+      if (this.resetForm) {
+        this.resetForm()
+      }
+
+      setTimeout(() => this.errors.clear(), 50)
+    },
+
+    /**
+     * Applies a red star at the end of a label for the required fields.
+     *
+     * @param label {String} - a text to whom the star is attached.
+     * @return {String} - the provided label + red star.
+     */
+    star(label) {
+      return `${label} <span style="color:red; vertical-align: sub; font-size: 18px;">*</span>`
     }
   }
 }
