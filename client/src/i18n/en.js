0b959a42a7b2829c46dc4b896e859f2a622d9054
paulbach <paulbachelerie@gmail.com>
2018-01-22 12:18:26 +0100

    [#26] - Add registration page

--- client/src/i18n/en.js
+++ client/src/i18n/en.js
@@ -1,16 +1,27 @@
 module.exports = {
   general: {
-    hello: 'Hello',
+    en: 'English',
+    fr: 'French',
     yes: 'Yes',
     no: 'No',
     login: 'Login',
     reset: 'Reset',
     register: 'Register',
-    forgot_password: 'Forgot Password'
+    forgot_password: 'Forgot Password',
+    language_changed: 'Language changed to: '
+  },
+  login: {
+    title: 'Login'
+  },
+  registration: {
+    title: 'Register',
+    registration_ok: 'Successfully registered!',
+    registration_failed: 'There were some errors durring the registration: '
   },
   attributes: {
     email: 'E-mail',
-    password: 'Password'
+    password: 'Password',
+    confirm_password: 'Confirm Password'
 
   }
 }
