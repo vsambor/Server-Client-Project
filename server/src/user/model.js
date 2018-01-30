a1500e033a5564ff7947eb8fd9e7458b6430c59a
Vasile Sambor <vasilesambor@gmail.com>
2018-01-30 09:56:42 +0100

    [#3] - Organize database structure for user model

--- server/src/user/model.js
+++ server/src/user/model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const { RoleEnum, VehicleEnum } = require('../util/enums')
+const i18n = require('../../config/i18n')
 
 const schema = mongoose.Schema({
   email: {
@@ -6,12 +8,59 @@ const schema = mongoose.Schema({
     trim: true,
     lowercase: true,
     unique: true,
-    required: 'Email address is required',
-    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    required: i18n.__('error.validation.email.required'),
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, i18n.__('error.validation.email.invalid')]
   },
-  password: { type: String, required: 'The password is required', minlength: [5, 'Password too short! (min 5 required)'] },
-  firstName: { type: String },
-  lastName: { type: String },
+  password: {
+    type: String,
+    required: i18n.__('error.validation.password.required'),
+
+    // This doesn't work properly; the check is done on save and because of bcrypt the length becames bigger. (we'll find a solution)
+    minlength: [5, i18n.__('error.validation.password.min_length', 5)]
+  },
+  role: {
+    type: String,
+    enum: {
+      values: RoleEnum,
+      message: i18n.__('error.validation.fieldType.enum', RoleEnum.join(', '))
+    },
+    default: RoleEnum[2]
+  },
+  profile: {
+    firstName: {
+      type: String,
+      match: [/(.+)/, i18n.__('error.validation.fieldType.string')]
+    },
+    lastName: { type: String },
+    age: {
+      type: Number,
+      validate: {
+        validator: Number.isInteger,
+        message: i18n.__('error.validation.fieldType.number')
+      },
+      min: 0
+    },
+    nationality: { type: String },
+    picture: { type: String }
+  },
+  vehicle: {
+    type: {
+      type: String,
+      enum: {
+        values: VehicleEnum,
+        message: i18n.__('error.validation.fieldType.enum', VehicleEnum.join(', '))
+      }
+    },
+    model: { type: String },
+    registrationNumber: { type: String },
+    color: { type: String }
+  },
+  settings: {
+    showNotification: { type: Boolean, default: true },
+
+    // Distance in km.
+    alertProximity: { type: Number, default: 10 }
+  },
   isActive: { type: Boolean, default: true }
 }, { timestamps: {} }) // The timestamps adds 2 fields; createdAt and updatedAt in the database.
 
