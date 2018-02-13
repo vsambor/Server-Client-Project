4d7dbdf38fcce6ec86a5857ce00810016220ece2
Vasile Sambor <vasilesambor@gmail.com>
2018-02-13 16:47:39 +0100

    [#50] - Fix populators

--- server/src/common/util/argUtil.js
+++ server/src/common/util/argUtil.js
@@ -21,6 +21,13 @@ exports.get = (cmd) => {
 }
 
 /**
+ * Checks if there is at least 1 populator command provided in node args.
+ */
+exports.hasAnyPopulator = () =>
+  getIndex('POPULATE_ADMIN') > -1 || getIndex('POPULATE_USER') > -1 ||
+  getIndex('POPULATE_ACCIDENTS') > -1 || getIndex('GENERATE_RDF') > -1
+
+/**
  * Returns the index of <cmd> in node process arguments list.
  *
  * @param {String} cmd - the command string to find the index for.
