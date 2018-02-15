f8ac2e88aec9b3421a3382204ee3edf398e5e1da
iliasnaamane <ilias.naamane1@gmail.com>
2018-02-16 00:10:51 +0100

    [#80] - Add back-end stats based on collisions

--- server/src/components/stats/service.js
+++ server/src/components/stats/service.js
@@ -1,5 +1,10 @@
 const sparql = require('../../common/service/semantic/sparql')
 
+/**
+ * Return a json response that contains all accidents groups by severity
+ * @param {*} req
+ * @param {*} res
+ */
 exports.countAllSeverities = (req, res) => {
   let dto = {}
 
@@ -19,6 +24,60 @@ exports.countAllSeverities = (req, res) => {
 }
 
 /**
+ * Return json resposne that contains number of accidents grouped by collision or not
+ * @param {*} req
+ * @param {*} res
+ */
+exports.countWithAndWithoutCol = (req, res) => {
+  let dto = {}
+  countCollisionsByKind(8).then((res1) => {
+    dto.withCollision = res1
+    countCollisionsByKind(7).then((res2) => {
+      dto.withoutCollision = res2
+      res.status(200).send(dto)
+    })
+  })
+}
+
+/**
+ * Return json response that contains number of accidents grouped by collision with vehicules or not
+ * @param {*} req
+ * @param {*} res
+ */
+exports.countWithColWithVehiAndWithout = (req, res) => {
+  let dto = {}
+  countCollisionsByKind(6).then((res1) => {
+    dto.otherCollision = res1
+    countCollisionsByKind(9).then((res2) => {
+      dto.vehiculeCollision = res2
+      res.status(200).send(dto)
+    })
+  })
+}
+
+/**
+ * Return json response that contains number of accidents grouped by collision with vehicules,
+ * two vehicules by side, by back, frontal....
+ * @param {*} req
+ * @param {*} res
+ */
+exports.countWithVehKinds = (req, res) => {
+  let dto = {}
+  countCollisionsByKind(4).then((res1) => {
+    dto.threeOrMore = res1
+    countCollisionsByKind(3).then((res2) => {
+      dto.twoVehiculesByTheSide = res2
+      countCollisionsByKind(2).then((res3) => {
+        dto.twoVehiculesByTheBack = res3
+        countCollisionsByKind(1).then((res3) => {
+          dto.twoVehiculesFrontal = res3
+          res.status(200).send(dto)
+        })
+      })
+    })
+  })
+}
+/**
  * Countes accidents by interval terminal spqrql query.
  * For the countExtremlySeriousAccidents we do not have to use two intervals.
  *
@@ -29,8 +88,25 @@ function countAccidents(interv1, interv2) {
   return new Promise((resolve, reject) => {
     sparql.queryRDF(`SELECT count(*)
       WHERE {
-        ?subject <http://xmlns.com/foaf/0.1/hasIndiceDeGravite> ?y
+        ?subject <http://www.webofdata.fr/prop#hasIndiceDeGravite> ?y
         FILTER(?y >= ${interv1}` + (interv2 ? `&& ?y < ${interv2})}` : ')}')
+    )
+      .then((result) => resolve(JSON.parse(result)['1'].results.bindings['0']['.1'].value))
+      .catch((err) => reject(err))
+  })
+}
+
+/**
+ * Counts accidents by kind of collision
+ *
+ * @param {*} kind
+ */
+function countCollisionsByKind(kind) {
+  return new Promise((resolve, reject) => {
+    sparql.queryRDF(`SELECT count(*)
+      WHERE {
+        ?subject <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.webofdata.fr/classes#collision${kind}> 
+       }`
     )
       .then((result) => resolve(JSON.parse(result)['1'].results.bindings['0']['.1'].value))
       .catch((err) => reject(err))
