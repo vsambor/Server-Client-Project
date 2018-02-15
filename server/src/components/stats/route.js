7f02bca21e50e3a75009103057ef1397320a90cc
iliasnaamane <ilias.naamane1@gmail.com>
2018-02-16 00:10:51 +0100

    [#80] - Add back-end stats based on collisions

--- server/src/components/stats/route.js
+++ server/src/components/stats/route.js
@@ -1,5 +1,8 @@
 const service = require('./service')
 
 module.exports = (app) => {
-  app.get('/api/semantic/accidents/stats/count', service.countAllSeverities)
+  app.get('/api/semantic/accidents/stats/count/severity', service.countAllSeverities)
+  app.get('/api/semantic/accidents/stats/count/collision', service.countWithAndWithoutCol)
+  app.get('/api/semantic/accidents/stats/count/collivehicules', service.countWithColWithVehiAndWithout)
+  app.get('/api/semantic/accidents/stats/count/collivehiculekind', service.countWithVehKinds)
 }
