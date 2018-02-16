211ef4e141cc53a968425c54daf3b795b8dea755
Vasile Sambor <vasilesambor@gmail.com>
2018-02-16 15:33:24 +0100

    [#89] - Add home page information

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-    <h4 class="title">Welcome to Server Client Project (STP)!</h4>
-    <div style="text-color: black;">{{ $i18n.t("general.yes") }}</div>
-    or
-    <div style="text-color: black;">{{ $i18n.messages.fr.general.no }}</div>
+    <div id="bg" class="fullscreen"> </div>
+
+    <h5 class="title">Welcome to Server Client Project (STP)!</h5>
+
+    <blockquote>
+      <p class="text-italic" v-html="$t('home.text')"></p>
+      <small v-html="$t('home.signature')"></small>
+    </blockquote>
+
   </div>
 </template>
 
@@ -12,6 +17,20 @@ export default {
   data() {
     return {}
   },
+
   methods: {}
 }
 </script>
+
+<style>
+#bg {
+  background-image: url('../../static/home-bg.jpg');
+  min-height: 500px;
+  background-size: cover;
+  -moz-background-size: cover; /* Firefox 3.6 */
+  background-position: center; /* Internet Explorer 7/8 */
+  opacity: 0.4;
+  z-index: -1;
+}
+</style>
+
