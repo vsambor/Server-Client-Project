ad0099b7a0ca280116a21aa08bad1921ec44ef89
Vasile Sambor <vasilesambor@gmail.com>
2018-02-16 15:33:50 +0100

    [#88] - Add about page information

--- client/src/components/public/About.vue
+++ client/src/components/public/About.vue
@@ -1,5 +1,11 @@
 <template>
-    <h4>About this app</h4>
+  <div>
+    <h5>{{$t('about.title')}}</h5>
+    <p v-html="$t('about.text')"></p>
+    <br>
+    <h5>{{$t('about.contact_title')}}</h5>
+    <p v-html="$t('about.contact_text')"></p>
+  </div>
 </template>
 
 <script>
