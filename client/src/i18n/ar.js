a44aad4b572375d4bdd967b19dfb54a00df2e40f
Bilal Koteich <bilal.koteich@gmail.com>
2018-02-20 23:52:12 +0100

    [#97] - Fix arabic language

--- client/src/i18n/ar.js
+++ client/src/i18n/ar.js
@@ -1,52 +1,190 @@
 module.exports = {
   general: {
     en: 'الإنجليزية',
-    fr: 'اللغة الفرنسية',
+    fr: 'الفرنسية',
     ro: 'الرومانية',
     ar: 'العربية',
+    ok: 'أجل',
     yes: 'نعم',
     no: 'كلا',
-    map: 'خريطة',
+    map: 'الخريطة',
     home: 'الصفحة الرئيسية',
-    dashboard: 'لوحة القيادة',
+    dashboard: 'Dashboard',
     profile: 'الملف الشخصي',
-    about: 'حول',
-    settings: 'إعدادات',
-    notifications: 'إخطارات',
-    back: 'الى الخلف',
-    cancel: 'إلغاء',
+    about: 'حول التطبيق',
+    settings: 'الإعدادات',
+    search_engine: 'محرّك البحث',
+    notifications: 'الإشعارات',
+    back: 'رجوع',
+    edit: 'تعديل',
+    delete: 'مسح',
+    save: 'حفظ',
+    cancel: 'الغاء',
+    new: 'جديد',
+    add: 'إضافة',
     login: 'تسجيل الدخول',
-    logout: 'الخروج',
+    logout: 'تسجيل الخروج',
     reset: 'مسح',
-    register: 'تسجيل',
-    forgot_password: 'هل نسيت كلمة المرور',
-    language_changed: 'تم تغيير اللغة إلى: ',
-    change_language: 'غير اللغة'
+    register: 'التسجيل',
+    forgot_password: 'نسيت كلمة المرور',
+    language_changed: 'تم تغيير اللغة الى : ',
+    change_language: 'تغيير اللغة',
+    change_password: 'تغيير كلمة المرور',
+    updated: 'تم التحديث',
+    role: 'دور',
+    added: 'تمت الاضافة',
+    users: 'المستخدمين',
+    start: 'البداية',
+    end: 'النهاية',
+    clear: 'مسح',
+    vehicles: 'السيارات',
+    search: 'بحث',
+    result: 'نتيجة'
   },
   login: {
     title: 'تسجيل الدخول',
-    login_ok: 'تم تسجيل الدخول بنجاح!',
+    login_ok: ':تم تسجيل الدخول بنجاح',
     login_failed: 'فشل تسجيل الدخول: '
   },
+  logout: {
+    success: 'تم تسجيل الخروج بنجاح'
+  },
   registration: {
-    title: 'تسجيل',
-    registration_ok: 'سجلت بنجاح!',
-    registration_failed: 'فشل في التسجيل: '
+    title: 'Register',
+    registration_ok: 'Successfully registered!',
+    registration_failed: 'Registration failed: '
   },
   attributes: {
-    email: 'البريد الإلكتروني',
-    password: 'كلمة المرور',
-    confirm_password: 'تأكيد كلمة المرور'
+    email: 'E-mail',
+    password: 'Password',
+    confirm_password: 'Confirm Password',
+    firstName: 'First Name',
+    lastName: 'Last Name',
+    age: 'Age',
+    nationality: 'Nationality'
+  },
+  accident: {
+    title: 'Accident',
+    add: 'Add accident',
+    total: 'Total accidents: {total}',
+    add_title: 'Add Accident',
+    add_message: 'The accident with location {location} will be created.',
+    severity: {
+      title: 'Severity',
+      minor: 'Minor',
+      moderate: 'Moderate',
+      serious: 'Serious',
+      xserious: 'Extremly Serious'
+    },
+    number: {
+      three_more: 'Three or more',
+      two_side: 'Two by side',
+      two_back: 'Two by back',
+      two_front: 'Two frontal'
+    },
+    date: 'Date',
+    details: {
+      title: 'Accident details',
+      dead: 'Dead',
+      seriouslyInjure: 'Seriously injury',
+      lightlyInjurie: 'Lightly injury',
+      unhurt: 'Unhurt',
+      light: 'Light',
+      organism: 'Organism',
+      intersection: 'Intersection',
+      weather: 'Weather',
+      crash: 'Crash',
+      road: 'Road',
+      infrastructure: 'Infrastructure',
+      nWay: 'Road number',
+      nVote: 'Votes Number',
+      specialWay: 'Special road',
+      heightDifference: 'Height difference',
+      shapeRoad: 'Shape road',
+      situationAccident: 'Accident situation '
+    }
+  },
+  comment: {
+    comment_accident: 'Comment accident',
+    comment: 'Comment',
+    no_comment: 'No comment',
+    edit_comment: 'Edit comment'
   },
   footer: {
-    rights: 'كل الحقوق محفوظة'
+    rights: 'All rights reserved'
   },
   settings: {
-    title: 'الاعدادات العامة',
-    geolocation: 'موقعي',
-    theme: 'موضوع التطبيق',
-    notification: 'إشعار التطبيق',
-    proximity: 'مسافة التحذير من الحوادث',
-    zoom: 'الخريطة الافتراضية التكبير'
+    title: 'General settings',
+    geolocation: 'My location',
+    theme: 'Application theme',
+    notification: 'Application notification',
+    alert_proximity: 'Accident alert warning distance',
+    avoid_proximity: 'Avoid accident radius',
+    zoom: 'Default map zoom'
+  },
+  map: {
+    start: 'Point A',
+    end: 'Point B',
+    avoid_accidents: 'Avoid Accidents',
+    controls_toggle: 'Toggle controls',
+    direction_toggle: 'Toggle directions',
+    directions_failed: 'Directions service failed: ',
+    arrived_at_destination: 'You have arrived at your destination'
+  },
+  profile: {
+    edit_title: 'Edit Profile',
+    vehicles_number: 'Number of vehicles'
+  },
+  side_menu: {
+    about_app: 'About app',
+    user_profile: 'User Profile',
+    user_settings: 'User Settings',
+    users_management: 'Users Management',
+    notifications_check: 'Check all notifications',
+    about_vehicles: 'Vehicles management',
+    search_engine: 'Semantic Search Engine'
+  },
+  notification: {
+    title: 'Notifications',
+    empty: 'No notifications available.'
+  },
+  vehicle: {
+    vehicle: 'vehicle',
+    type: 'Type',
+    model: 'Model',
+    registration_number: 'Registration number',
+    color: 'Color'
+  },
+  confirm: {
+    title: 'Confirmation',
+    generic_question: 'Are you sure?',
+    remove_notifications: 'Are you sure you want to remove the selected notifications?'
+  },
+  stats: {
+    title1: 'Accidents number by severity',
+    title2: 'Accidents number by collision\'s kind',
+    title3: 'Accidents number by collision',
+    title4: 'Accidents number by collisios with vehicules',
+    accidents_collision: 'Accidents with collision',
+    accidents_no_collision: 'Accidents without collision',
+    accidents_vehicles_colision: 'Accidents with collision with vehicules',
+    accidents_other_vehicukes: 'Accidents with other vehicules',
+    accidents_vehicles_not: 'Accidents with vehicles or not'
+  },
+  search_engine: {
+    subject_placeholder: 'query subject here',
+    property_placeholder: 'query property here',
+    property_value_placeholder: 'the property value',
+    condition_placeholder: 'query conditions separated by \';\' (ex: > 10; < 5;)'
+  },
+  home: {
+    text: 'SCP is an application which allows it\'s users to see in real time, on the map all the accidents which take place in the user proximity.<br> As a user of our application, you will be real time notified when new actions happens.<br> SCP uses only official, governamental data from incidents reported to the police so the results are the most reliable available. We aim to keep the map as up-to-date as possible.<br><br>Please sign up by clicking on register button in the header bar and we guaranty that will have a great experience.',
+    signature: 'The BIPV - CEO, for our respected <i>SCP users</i>'
+  },
+  about: {
+    title: 'About this app',
+    contact_title: 'Contact',
+    contact_text: 'If you want to get in touch with us or you had encountered any issue, feel free to contac us by email: <b>admin@scp.com</b>',
+    text: 'This application allows it\'s users to see in real time all the accidents which take place in their proximity.'
   }
 }
