6e57ee90e85b9c8b9731563934c638d6fdd6082b
Vasile Sambor <vasilesambor@gmail.com>
2018-02-20 23:51:00 +0100

    [#97] - Fix roumanian language

--- client/src/i18n/ro.js
+++ client/src/i18n/ro.js
@@ -4,49 +4,187 @@ module.exports = {
     fr: 'Franceză',
     ro: 'Română',
     ar: 'Arabă',
+    ok: 'OK',
     yes: 'Da',
     no: 'Nu',
     map: 'Hartă',
     home: 'Acasă',
-    dashboard: 'tablou de bord',
+    dashboard: 'Tablou de bord',
     profile: 'Profil',
     about: 'Despre',
     settings: 'Setări',
+    search_engine: 'Motor de căutare',
     notifications: 'Notificări',
     back: 'Înapoi',
-    cancel: 'Anulare',
-    login: 'Conectare',
-    logout: 'Deconectare',
+    edit: 'Editare',
+    delete: 'Șterge',
+    save: 'Salvează',
+    cancel: 'Cancel',
+    new: 'Nou',
+    add: 'Adaugă',
+    login: 'Logare',
+    logout: 'Delogare',
     reset: 'Resetează',
-    register: 'Înregistrare',
+    register: 'Registrează',
     forgot_password: 'Parolă uitată',
     language_changed: 'Limba schimbată în: ',
-    change_language: 'Schimbă Limba'
+    change_language: 'Schimbă Limba',
+    change_password: 'Schimbă parola',
+    updated: 'Updatat',
+    role: 'Rol',
+    added: 'Adăugat',
+    users: 'Utilizatori',
+    start: 'Start',
+    end: 'Final',
+    clear: 'Curăță',
+    vehicles: 'Vehicule',
+    search: 'Caută',
+    result: 'Resultat'
   },
   login: {
     title: 'Conectare',
-    login_ok: 'Logat cu succes!',
+    login_ok: 'Conectat cu succes!',
     login_failed: 'Probleme la logare: '
+  },
+  logout: {
+    success: 'Ieșire din aplicație cu succes!'
   },
   registration: {
     title: 'Înregistrare',
     registration_ok: 'Înregistrat cu succes!',
-    registration_failed: 'Probleme la înregistrare: '
+    registration_failed: 'Registrare cu success: '
   },
   attributes: {
     email: 'E-mail',
-    password: 'Parolă',
-    confirm_password: 'Confirmă Parola'
+    password: 'Parolî',
+    confirm_password: 'Confirmă parola',
+    firstName: 'Prenume',
+    lastName: 'Nume',
+    age: 'Vârstă',
+    nationality: 'Naționalitate'
+  },
+  accident: {
+    title: 'Accident',
+    add: 'Adaugă accident',
+    total: 'Total accidente: {total}',
+    add_title: 'Adaugă Accident',
+    add_message: 'Accident adăugat la locația {location} va fi creat.',
+    severity: {
+      title: 'Severitate',
+      minor: 'Minor',
+      moderate: 'Moderat',
+      serious: 'Serios',
+      xserious: 'Extrem de Serios'
+    },
+    number: {
+      three_more: 'Trei sau mai multe',
+      two_side: 'Două laterale',
+      two_back: 'Două în spate',
+      two_front: 'Două frontale'
+    },
+    date: 'Dată',
+    details: {
+      title: 'Detalii accident',
+      dead: 'Morți',
+      seriouslyInjure: 'Seriously injury',
+      lightlyInjurie: 'Lightly injury',
+      unhurt: 'Unhurt',
+      light: 'Light',
+      organism: 'Organism',
+      intersection: 'Intersection',
+      weather: 'Weather',
+      crash: 'Crash',
+      road: 'Road',
+      infrastructure: 'Infrastructure',
+      nWay: 'Road number',
+      nVote: 'Votes Number',
+      specialWay: 'Special road',
+      heightDifference: 'Height difference',
+      shapeRoad: 'Shape road',
+      situationAccident: 'Accident situation '
+    }
+  },
+  comment: {
+    comment_accident: 'Comment accident',
+    comment: 'Comment',
+    no_comment: 'No comment',
+    edit_comment: 'Edit comment'
   },
   footer: {
-    rights: 'Toate drepturile rezervate'
+    rights: 'All rights reserved'
   },
   settings: {
-    title: 'Setari generale',
-    geolocation: 'Locatia mea',
-    theme: 'Tema aplicației',
-    notification: 'Aplicație notificare',
-    proximity: 'Distanta de avertizare a distantei',
-    zoom: 'Zoom implicit pe hartă'
+    title: 'General settings',
+    geolocation: 'My location',
+    theme: 'Application theme',
+    notification: 'Application notification',
+    alert_proximity: 'Accident alert warning distance',
+    avoid_proximity: 'Avoid accident radius',
+    zoom: 'Default map zoom'
+  },
+  map: {
+    start: 'Point A',
+    end: 'Point B',
+    avoid_accidents: 'Avoid Accidents',
+    controls_toggle: 'Toggle controls',
+    direction_toggle: 'Toggle directions',
+    directions_failed: 'Directions service failed: ',
+    arrived_at_destination: 'You have arrived at your destination'
+  },
+  profile: {
+    edit_title: 'Edit Profile',
+    vehicles_number: 'Number of vehicles'
+  },
+  side_menu: {
+    about_app: 'About app',
+    user_profile: 'User Profile',
+    user_settings: 'User Settings',
+    users_management: 'Users Management',
+    notifications_check: 'Check all notifications',
+    about_vehicles: 'Vehicles management',
+    search_engine: 'Semantic Search Engine'
+  },
+  notification: {
+    title: 'Notifications',
+    empty: 'No notifications available.'
+  },
+  vehicle: {
+    vehicle: 'vehicle',
+    type: 'Type',
+    model: 'Model',
+    registration_number: 'Registration number',
+    color: 'Color'
+  },
+  confirm: {
+    title: 'Confirmation',
+    generic_question: 'Are you sure?',
+    remove_notifications: 'Are you sure you want to remove the selected notifications?'
+  },
+  stats: {
+    title1: 'Accidents number by severity',
+    title2: 'Accidents number by collision\'s kind',
+    title3: 'Accidents number by collision',
+    title4: 'Accidents number by collisios with vehicules',
+    accidents_collision: 'Accidents with collision',
+    accidents_no_collision: 'Accidents without collision',
+    accidents_vehicles_colision: 'Accidents with collision with vehicules',
+    accidents_other_vehicukes: 'Accidents with other vehicules',
+    accidents_vehicles_not: 'Accidents with vehicles or not'
+  },
+  search_engine: {
+    subject_placeholder: 'query subject here',
+    property_placeholder: 'query property here',
+    property_value_placeholder: 'the property value',
+    condition_placeholder: 'query conditions separated by \';\' (ex: > 10; < 5;)'
+  },
+  home: {
+    text: 'SCP is an application which allows it\'s users to see in real time, on the map all the accidents which take place in the user proximity.<br> As a user of our application, you will be real time notified when new actions happens.<br> SCP uses only official, governamental data from incidents reported to the police so the results are the most reliable available. We aim to keep the map as up-to-date as possible.<br><br>Please sign up by clicking on register button in the header bar and we guaranty that will have a great experience.',
+    signature: 'The BIPV - CEO, for our respected <i>SCP users</i>'
+  },
+  about: {
+    title: 'About this app',
+    contact_title: 'Contact',
+    contact_text: 'If you want to get in touch with us or you had encountered any issue, feel free to contac us by email: <b>admin@scp.com</b>',
+    text: 'This application allows it\'s users to see in real time all the accidents which take place in their proximity.'
   }
 }
